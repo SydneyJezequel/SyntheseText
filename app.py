cd891d96a093de8f5859d399ae31e94f47835619
SydneyJezequel <107682562+SydneyJezequel@users.noreply.github.com>
2026-02-17 23:26:17 +0100

    commit initial

---
+++ app.py
@@ -0,0 +1,40 @@
+import cohere
+import streamlit as st
+import config
+
+
+
+# Commande pour lancer l'application :
+# streamlit run app.py
+
+
+# Initialisation de cohere :
+co = cohere.ClientV2(config.API_KEY_COHERE)
+
+
+# Interface utilisateur :
+st.title("Résumé de texte avec Cohere.")
+
+st.write("Cette application utilise une API Cohere pour résumer du texte. Renseigner votre texte à résumer ci-dessous.")
+
+user_input = st.text_area("Renseigner votre texte ici :", height = 300)
+if st.button("Résumer ce texte"):
+    if user_input.strip():
+        with st.spinner("Résumé en cours"):
+            try:
+                # Préparation du prompt :
+                message =f"Generate a concise summary of the following text/n{user_input}"
+                # Appel API :
+                response = co.chat(
+                    model="command-r-plus-08-2024",
+                    messages=[{"role":"user","content":message}]
+                )
+                # Affichage du texte résumé :
+                resume = response.message.content[0].text
+                st.subheader("Résumé de texte.")
+                st.write(resume)
+            except Exception as e :
+                st.error(f"An error occurred : {e}")
+    else:
+        st.warning("Merci d'entrer un texte à résumer.")
+
